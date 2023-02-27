12861974e368e25351168198ad95c140ff1f72a7
Dmitry Kodolbenko <kodolbenko.1@yandex.ru>
2023-02-27 17:06:43 +0300

    Все правки, кроме css

---
+++ scripts/index.js
@@ -0,0 +1,55 @@
+let popup = document.querySelector('.popup');
+let editButton = document.querySelector('.profile__edit-button');
+let popupCloseButton = document.querySelector('.popup__close-icon');
+
+// Выберите элементы, куда должны быть вставлены значения полей
+let title = document.querySelector('.profile__title');
+let subtitle = document.querySelector('.profile__subtitle');
+
+
+function popupClose() {
+  popup.classList.remove('popup_opened');
+}
+
+function popupOpen() {
+  popup.classList.add('popup_opened');
+  nameInput.value = title.textContent;
+  jobInput.value = subtitle.textContent;
+}
+
+editButton.addEventListener('click', popupOpen);
+popupCloseButton.addEventListener('click', popupClose);
+
+
+// save button
+
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__form');
+// Находим поля формы в DOM
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_job');
+
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormSubmit(evt) {
+
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Так мы можем определить свою логику отправки.
+  // О том, как это делать, расскажем позже.
+
+  // Получите значение полей jobInput и nameInput из свойства value
+  nameValue = nameInput.value;
+  jobValue = jobInput.value;
+
+
+  // Вставьте новые значения с помощью textContent
+  title.textContent = nameValue;
+  subtitle.textContent = jobValue;
+
+  popupClose();
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit);
